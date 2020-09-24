95a08cd8fc19965b0ac8ba628ed26cc0739620c8
Volmar Kaldo <volmar.kaldo@learnet.se>
2020-09-24 11:07:42 +0200

    Början på projektet.

---
+++ Kapitel-3/Projekt-3-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Projekt_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int svar = 0;
+            int pigghet = 0;
+            Console.WriteLine("Det är den sista veckan innan höstlovet, och självklart innebär detta minst ett prov i varända ämne. Först är det engelska måndag, sen tisdag är det svenska och matematik. Onsdag är det äntligen avslutning, men inte förrän efter ytterligare ett")
+            Console.WriteLine("Du vaknar som vanligt en vanlig torsdagsmorgon av att ditt larm ringer. Du sträcker dig för att stänga av larmet.");
+            Console.WriteLine("1. Du väljer att stänga av larmet och gå upp.");
+            System.Console.WriteLine("2. Du väljer att snooza larmet i tio minuter.");
+        }
+    }
+}
