713246ff27ab09557667e320b730d78c84c856fc
Volmar Kaldo <volmar.kaldo@learnet.se>
2020-11-11 14:32:33 +0100

    Lektion 2020 11 11

---
+++ Kapitel-4/Uppgift-Lyckohjul/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Uppgift_Lyckohjul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gissning;
+            int slumptal;
+            string input;
+            Random slumpTal;
+
+            Console.WriteLine("Gissa ett tal mellan 1 och 10");
+            input = Console.ReadLine();
+            int.TryParse(input, out gissning);
+
+            slumpTal = new Random();
+            slumptal = slumpTal.Next(1, 11);
+
+            System.Console.WriteLine($"Slumptalet blev {slumptal}");
+
+            if (gissning == slumptal)
+            {
+                System.Console.WriteLine("Du vann! Grattis!");
+            }
+            else System.Console.WriteLine("Du förlorade. Va synd.");
+        }
+    }
+}
